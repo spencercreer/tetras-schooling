8b41a4a095570f8d8c1339bd81596c0702683670
spencercreer <spencercreer@gmail.com>
2021-08-24 11:04:30 -0700

    added Session notes inquirer and updated txt

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
+// import student data from _students.txt, seperate the students, convert each object to JSON, create a names array for inquirer
+var students = fs.readFileSync('_students.txt', 'utf8')
+var studentsInfoArray = students.split('\r\n')
+studentsInfoArray = studentsInfoArray.map(i => JSON.parse(i))
+var studentsNamesArray = []
+studentsInfoArray.forEach(student => {
+    studentsNamesArray.push(`${student.id}-${student.firstName} ${student.lastName}`)
+})
+
 function init() {
-    var gMd = require('./utils/generateMarkdown')
-    inquirer.prompt(questions).then(response =>
-        fs.writeFile('tutor-template', gMd(respons, (err) =>
-            err ? console.error(err) : console.log(`--------------\tutor-template generated\n--------------`))
-        )
-    )
+    var gTxt = require('./utils/generateTxt')
+    inquirer.prompt(initialQuestion).then(response => {
+        var init = response.init
+        // get the id of the selected student and find all of the selected students info
+        var studentId = response.student.split('-')[0]
+        var selectedStudent = studentsInfoArray.find(student => student.id === parseInt(studentId))
+
+        if(init === 'Email confirmation') {
+            inquirer.prompt(emailQuestions).then(response => {
+                // add inquirer data to students info
+                selectedStudent.date = response.date
+                selectedStudent.time = response.time
+                selectedStudent.init = init
+                // generate .txt file with file system
+                console.log(selectedStudent)
+                fs.writeFile('_tutor-template.txt', gTxt(selectedStudent), (err) =>
+                    err ? console.error(err) : console.log(`----------------------\n_tutor-template.txt\n----------------------`))
+            })
+        } else {
+              // generate .txt file with file system
+              fs.writeFile('_tutor-template.txt', gTxt(selectedStudent), (err) =>
+              err ? console.error(err) : console.log(`----------------------\n_tutor-template.txt\n----------------------`))
+        }
+    })
 }
 
-const questions = [
+// initial question for inquirer
+const initialQuestion = [
     {
         type: 'list',
-        message: "Select tutor email template.",
-        choices: ['Introductory', 'Confirmation', 'Stand-in', 'Reassignment', 'Blast'],
-        name: 'template',
+        message: 'What would you like to do?',
+        choices: ['Email confirmation','Session notes'],
+        name:'init'
     },
     {
-        type: 'input',
-        message: "What is the student's first and last name?",
-        name: 'full-name',
+        type: 'list',
+        message: "Select a Student",
+        choices: studentsNamesArray,
+        name: 'student'
     },
+]
+
+// emailQuestions for inquirer
+const emailQuestions = [
     {
         type: 'input',
-        message: 'What is the Date of the scheduled session?',
+        message: 'What is the date of the scheduled session?',
         name: 'date',
     },
     {
         type: 'input',
-        message: 'Enter the scheduled session time',
+        message: 'Enter the scheduled session time in Arizona time (Enter in military time as 00:00)',
         name: 'time',
     },
-    {
-        type: 'list',
-        message: "What is the students time zone?",
-        choices: ['Eastern Daylight Time', 'Eastern Standard Time', 'Central Daylight Time', 'Central Standard Time', 'Mountain Daylight Time', 'Mountain Standard Time', 'Pacific Daylight Time', 'Pacific Standard Time'],
-        name: 'time-zone'
-    }
 ]
 
 init()
---
+++ utils/generateTxt.js
@@ -0,0 +1,41 @@
+function generateTxt(data) {
+    let txt
+    if(data.init === 'Email confirmation') {
+        let hour = parseInt(data.time.split(':')[0])
+        let minute = data.time.split(':')[1]
+        let meridiem
+        // Adjust hour for student's time zone
+        if(data.timeZone === 'Mountain Time') {
+            hour += 1
+            console.log(hour)
+
+        } else if(data.timeZone === 'Central Time') {
+            hour += 2
+            console.log(hour)
+
+        }
+    
+        // Convert military time to am/pm time
+        if(hour > 12) {
+            hour -= 12
+            meridiem = 'pm'
+        } else if(hour === 12) {
+            meridiem = 'pm'
+        } else {
+            meridiem = 'am'
+        }
+    
+        txt = `# Subject
+Coding Boot Camp - Tutorial Confirmation - ${data.date}, ${hour}:${minute}${meridiem} ${data.timeZone}
+            
+# Email_Body
+Hi ${data.firstName}!
+    
+Thank you for scheduling your session with me. I am looking forward to our session on ${data.date}, ${hour}:${minute}${meridiem} ${data.timeZone}.`
+    } else (
+    txt = `Hello`
+    )
+    return txt
+}
+
+module.exports = generateTxt
