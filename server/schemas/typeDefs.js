37d9906538ee010515c992abeb9d6c58b7d5eddf
spencercreer <spencercreer@gmail.com>
2022-02-23 20:18:32 -0700

    updated Student typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,12 +2,19 @@ const { gql } = require('apollo-server-express')
 
 const typeDefs = gql`
     type Student {
-        _id: ID!
+        id: ID!
         first_name: String
+        last_name: String
+        email: String
+        class_code: String
+        grad_date: String
+        time_zone: String
+        slack: String
+        status: String
     }
 
     type Query {
-        getStudent: Student
+        getStudent: [Student]
     }
 `
 
