5554882f0e47b661cbd1099f10dd6a745d0650ae
spencercreer <spencercreer@gmail.com>
2022-03-27 22:04:34 -0700

    slack button

--- client/src/components/StudentModal.js
+++ client/src/components/StudentModal.js
@@ -1,25 +1,49 @@
 import { useQuery } from '@apollo/client'
 import { GET_STUDENT_MODAL } from '../utils/queries';
 
-import { Modal, Avatar } from 'antd'
+import { Modal, Button, Avatar } from 'antd'
+import { SlackOutlined } from '@ant-design/icons';
+
 
 const StudentModal = ({ visible, handleCancel, studentId }) => {
     console.log(studentId)
 
     const { loading, data } = useQuery(GET_STUDENT_MODAL, { variables: { id: studentId } })
-    if (loading) 
+    if (loading)
         return <div>Loading...</div>
 
     const { first_name, last_name, email, class_code, grad_date, time_zone, slack } = data?.getStudent
 
-    const handleOk = () => {
-        console.log('ok')
-        //   setIsModalVisible(false);
+    const handleSlackClick = () => {
+       navigator.clipboard.writeText(`Please fill out the evaluation form at the link below:
+https://docs.google.com/a/trilogyed.com/forms/d/e/1FAIpQLSdb4ejjbqoqKO-Q4k7zeO_xwykwB0dxYLWYm1mX5Ik45MzEeg/viewform
+
+Your class code is: ${class_code}`)
     };
 
     return (
         <>
-            <Modal title="Student Info" visible={visible} onOk={handleOk} onCancel={handleCancel}>
+            <Modal
+                title="Student Info"
+                visible={visible}
+                // onOk={handleOk}
+                onCancel={handleCancel}
+                footer={[
+                    <Button key="back" onClick={handleCancel}>
+                        Return
+                    </Button>,
+                    <Button key="submit" type="primary" loading={loading}>
+                        Submit
+                    </Button>,
+                    <Button
+                        type="primary"
+                        loading={loading}
+                        onClick={handleSlackClick}
+                    >
+                        <SlackOutlined />
+                    </Button>,
+                ]}
+            >
                 <Avatar style={{ backgroundColor: '#00a2ae' }}>{first_name[0] + last_name[0]}</Avatar>
                 <h2>{`${first_name}  ${last_name}`}</h2>
                 <a href={email} target="_blank" rel="noopener noreferrer">{email}</a>
