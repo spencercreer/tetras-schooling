dd290bfbfef4a478418795a5ac18139ed5cc86f4
spencercreer <spencercreer@gmail.com>
2022-05-02 22:40:29 -0700

    prework session

--- client/src/pages/sessions/components/SessionModal.js
+++ client/src/pages/sessions/components/SessionModal.js
@@ -1,11 +1,11 @@
 // React
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 // Apollo
 import { useQuery, useMutation } from '@apollo/client'
 import { GET_SESSION } from '../../../utils/queries'
 import { UPDATE_SESSION } from '../../../utils/mutations'
 // Antd
-import { Modal, Form, Button, Radio, Select, Input, TimePicker, Slider, message, Tooltip } from 'antd'
+import { Modal, Form, Button, Radio, Select, Input, TimePicker, message, Tooltip } from 'antd'
 import { SlackOutlined, ClockCircleOutlined } from '@ant-design/icons'
 // Utils
 import { layout, validateMessages, topics } from '../../../utils/form'
@@ -18,6 +18,7 @@ const { Option } = Select
 const { TextArea } = Input;
 
 const SessionModal = ({ visible, handleCloseModal, sessionId }) => {
+    const [preworkSession, setPreworkSession] = useState(false)
     const [form] = Form.useForm()
     useEffect(() => {
         form.resetFields()
@@ -32,7 +33,7 @@ const SessionModal = ({ visible, handleCloseModal, sessionId }) => {
     const timeZone = formatTimeZone(time_zone)
     const sessionDate = convertDate(date, 'llll')
 
-    const onFinish = async ({ b2b, clock_out, show, topics, effort, notes }) => {
+    const onFinish = async ({ prework, b2b, clock_out, show, topics, effort, notes }) => {
         const { data } = await updateSession({
             variables: { sessionData: { id, b2b, clock_out, tutor_eval: true, show, topics, notes } }
         })
@@ -53,7 +54,7 @@ const SessionModal = ({ visible, handleCloseModal, sessionId }) => {
             .then(() => navigator.clipboard.writeText(getClockOutNotes(class_code, first_name, last_name, B2B, show)))
             .then(() => message.success('Clock-out notes copied! ' + getRandomEmoji(), .7))
             .then(() => message.loading('Opening Form', 1))
-            .then(() => window.open(`https://docs.google.com/forms/d/e/1FAIpQLSc_q0CSp5Bpn7lfDAdoPCbBTW-OxWQVhC3gG5P9e6iE4FERjw/viewform?entry.1626809215=${class_code}&entry.1262798942=${last_name}, ${first_name}&entry.1509111758=${email}&entry.1450620354=No&entry.758887222=Creer, Spencer&entry.1572772860=Yes&entry.568333504=FSF - Full Stack Flex Web Development(Javascript)&entry.1311659485=${B2B}&entry.401287639=${today.formatted}&entry.781752343_hour=${clockIn.hour}&entry.781752343_minute=${clockIn.minute}&entry.721200944_hour=${clockOut.hour}&entry.721200944_minute=${clockOut.minute}&entry.1394734474=No&entry.2041303987=${topics}&entry.790082012=I am not a TA in this student's class&entry.2075286046=${effort}&entry.1836903312=No mention of it at all.&entry.2058615286=${notes}`, 'noreferrer'))
+            .then(() => window.open(`https://docs.google.com/forms/d/e/1FAIpQLSc_q0CSp5Bpn7lfDAdoPCbBTW-OxWQVhC3gG5P9e6iE4FERjw/viewform?entry.1626809215=${class_code}&entry.1262798942=${last_name}, ${first_name}&entry.1509111758=${email}&entry.1450620354=No&entry.758887222=Creer, Spencer&entry.1572772860=Yes&entry.568333504=FSF - Full Stack Flex Web Development(Javascript)&entry.1311659485=${B2B}&entry.401287639=${today.formatted}&entry.781752343_hour=${clockIn.hour}&entry.781752343_minute=${clockIn.minute}&entry.721200944_hour=${clockOut.hour}&entry.721200944_minute=${clockOut.minute}&entry.1394734474=${prework}&entry.2041303987=${topics}&entry.790082012=I am not a TA in this student's class&entry.2075286046=${effort}&entry.1836903312=No mention of it at all.&entry.2058615286=${notes}`, 'noreferrer'))
     }
 
     const handleSlackClick = () => {
@@ -108,25 +109,30 @@ const SessionModal = ({ visible, handleCloseModal, sessionId }) => {
                     onFinish={onFinish}
                     validateMessages={validateMessages}
                     initialValues={{
+                        prework: 'No',
                         b2b: false,
                         show: true,
                         effort: "5"
                         //  clock_in: moment(convertDate(date, 'HH:mm a', 0).formatted, 'HH:mm a')
                     }}
                 >
+                    <Item name={'prework'} label='Prework' rules={[{ required: true }]}>
+                        <Radio.Group onChange={() => setPreworkSession(!preworkSession)}>
+                            <Radio.Button value={'No'}>No</Radio.Button>
+                            <Radio.Button value={'Yes'}>Yes</Radio.Button>
+                        </Radio.Group>
+                    </Item>
+                    {preworkSession &&
+                        <Item name={'prework_topics'} label='Prework Topics' rules={[{ required: true }]}>
+                            <Input />
+                        </Item>
+                    }
                     <Item name={'show'} label='Show' rules={[{ required: true }]}>
                         <Radio.Group>
                             <Radio.Button value={true}>Show</Radio.Button>
                             <Radio.Button value={false}>No Show</Radio.Button>
                         </Radio.Group>
                     </Item>
-                    {/* <Item name={'clock_in'} label='Clock-in' rules={[{ required: true }]}
-                    >
-                        <TimePicker
-                            format='HH:mm a'
-                        // disabled={true}
-                        />
-                    </Item> */}
                     <Item name={'clock_out'} label='Clock-Out' rules={[{ required: true }]}
                     >
                         <TimePicker
