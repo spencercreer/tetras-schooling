50900d97387c635524a06c5e68ae07d3af6a357c
spencercreer <spencercreer@gmail.com>
2022-05-03 21:55:33 -0700

    cleanup session messages

--- client/src/pages/sessions/components/SessionModal.js
+++ client/src/pages/sessions/components/SessionModal.js
@@ -10,7 +10,7 @@ import { SlackOutlined, ClockCircleOutlined } from '@ant-design/icons'
 // Utils
 import { layout, validateMessages, topics } from '../../../utils/form'
 import { convertDate, formatTimeZone } from '../../../utils/conversions'
-import { getClockOutNotes, getSlackMessage, getRandomEmoji } from '../../../utils/messages'
+import { getClockOutNotes, getSlackMessage, getFormUrl, getSessionMessages, getRandomEmoji } from '../../../utils/messages'
 // import moment from 'moment'
 
 const { Item } = Form
@@ -29,32 +29,25 @@ const SessionModal = ({ visible, handleCloseModal, sessionId }) => {
         return <div>Loading...</div>
 
     const { id, date, Student: { first_name, last_name, email, class_code, grad_date, time_zone } } = data?.getSession
+    const session = data?.getSession
     // TODO: This is doubled on SessionCard
     const timeZone = formatTimeZone(time_zone)
     const sessionDate = convertDate(date, 'llll')
 
-    const onFinish = async ({ prework, b2b, clock_out, show, topics, effort, notes }) => {
+    const onFinish = async (values) => {
+        const { b2b, clock_out, show, topics, notes } = values
         const { data } = await updateSession({
             variables: { sessionData: { id, b2b, clock_out, tutor_eval: true, show, topics, notes } }
         })
         // TODO: Load the session modal with data if the session has already been recorded
-        const gradDate = convertDate(grad_date, 'L')
-        const today = convertDate(Date.now(), 'YYYY-MM-DD')
-        const { diff } = formatTimeZone(time_zone)
-        const clockIn = convertDate(date, 'LT ', 0)
-        const clockOut = convertDate(data.updateSession.clock_out, 'LT', 0)
-        console.log(clockOut.hour, clockOut.minute)
-        const B2B = b2b ? 'Yes' : 'No'
-        const SHOW = show ? 'Show' : 'No Show'
-        //TODO: Add presession conf
+        const { googleSheet, clockOutNotes, formUrl } = getSessionMessages(data, values, session)
 
-        //TODO: Automate this process
-        navigator.clipboard.writeText(`=SPLIT("${class_code},${gradDate.formatted},${first_name} ${last_name},${email},${today.formatted},+${diff}hr,${clockIn.formatted},${clockOut.formatted},Yes, ${B2B},Yes, ${SHOW},${topics},${notes},Yes", ",")`)
+        navigator.clipboard.writeText(googleSheet)
             .then(() => message.success(`Google sheets row copied! ` + getRandomEmoji(), .7))
-            .then(() => navigator.clipboard.writeText(getClockOutNotes(class_code, first_name, last_name, B2B, show)))
+            .then(() => navigator.clipboard.writeText(clockOutNotes))
             .then(() => message.success('Clock-out notes copied! ' + getRandomEmoji(), .7))
             .then(() => message.loading('Opening Form', 1))
-            .then(() => window.open(`https://docs.google.com/forms/d/e/1FAIpQLSc_q0CSp5Bpn7lfDAdoPCbBTW-OxWQVhC3gG5P9e6iE4FERjw/viewform?entry.1626809215=${class_code}&entry.1262798942=${last_name}, ${first_name}&entry.1509111758=${email}&entry.1450620354=No&entry.758887222=Creer, Spencer&entry.1572772860=Yes&entry.568333504=FSF - Full Stack Flex Web Development(Javascript)&entry.1311659485=${B2B}&entry.401287639=${today.formatted}&entry.781752343_hour=${clockIn.hour}&entry.781752343_minute=${clockIn.minute}&entry.721200944_hour=${clockOut.hour}&entry.721200944_minute=${clockOut.minute}&entry.1394734474=${prework}&entry.2041303987=${topics}&entry.790082012=I am not a TA in this student's class&entry.2075286046=${effort}&entry.1836903312=No mention of it at all.&entry.2058615286=${notes}`, 'noreferrer'))
+            .then(() => window.open(formUrl, 'noreferrer'))
     }
 
     const handleSlackClick = () => {
